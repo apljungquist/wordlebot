0d003d30934355f54bf9d26f0f46b8015776ee99
AP Ljungquist <ap@ljungquist.eu>
2022-01-15 20:32:58 +0100

    Exploit more information from yellow letters
    At this point it is not clear to me how, or indeed that, more options
    can be eliminated.
    
    Uses 5.20 guesses/word.

--- src/wordlebot/guessing.py
+++ src/wordlebot/guessing.py
@@ -9,12 +9,26 @@ def _fmt_permitted(permitted):
     return "\n".join("".join(c if c in p else " " for c in ALPHABET) for p in permitted)
 
 
+def _is_option(word, permitted, required):
+    for c, p in zip(word, permitted):
+        if c not in p:
+            return False
+
+    counts = collections.Counter(word)
+    for c in required:
+        if counts[c] < required[c]:
+            return False
+
+    return True
+
+
 def _options(state, wordlist):
     """Return (superset of) possible answers"""
     # Superset because the information from the state may not be fully exploited
     permitted = [set(ALPHABET) for _ in range(5)]
     required = collections.defaultdict(int)
     for guess, feedback in [step.split(":") for step in state.split(",")]:
+        marginal_required = collections.defaultdict(int)
         for i, (g, f) in enumerate(zip(guess, feedback)):
             f = int(f)
             match f:
@@ -22,23 +36,25 @@ def _options(state, wordlist):
                     assert g == "-"
                 case 1:
                     permitted[i].discard(g)
-                    if g not in required:
+                    # If a letter occurs multiple times in a guess but only once in the
+                    # answer, only the first occurrence will be scored as a two.
+                    if g not in marginal_required:
                         for p in permitted:
                             p.discard(g)
                 case 2:
-                    required[g] = 1
+                    marginal_required[g] += 1
                     permitted[i].discard(g)
                 case 3:
-                    required[g] = 1
+                    marginal_required[g] += 1
                     permitted[i] = {g}
                 case _:
                     assert False
 
+        for k, v in marginal_required.items():
+            required[k] = max(required[k], v)
+
     for word in wordlist:
-        for c, p in zip(word, permitted):
-            if c not in p:
-                break
-        else:
+        if _is_option(word, permitted, marginal_required):
             yield word
 
 
